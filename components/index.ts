28a3ef71270b103e76caf30769153af0f4eeead3
Avirup Ghosh <ag.avirup1997@gmail.com>
2022-07-19 14:16:54 +0530

    added basic components
    - Anchor
    - Icon
    - Header & Footer
    - SEO component

---
+++ components/index.ts
@@ -0,0 +1,9 @@
+export { default as Head } from 'next/head'
+export { default as Image } from 'next/future/image'
+export { default as Script } from 'next/script'
+
+export { default as Anchor } from './Anchor'
+export { default as Footer } from './Footer'
+export { default as Header } from './Header'
+export { default as Icon } from './Icon'
+export { default as SEO } from './SEO'
