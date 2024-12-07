6fe9ce8c9cf336eb6c764f4661857093aec5c108
Avirup Ghosh <ag.avirup1997@gmail.com>
2024-12-08 01:28:53 +0530

    📦 using 'get' library for https requests

--- src/uup/request.ts
+++ src/uup/request.ts
@@ -1,7 +1,16 @@
-export function uupPostRequest(url: string, body: string) {
-  const headers = {
-    'User-Agent': 'Windows-Update-Agent/10.0.10011.16384 Client-Protocol/2.50',
-    'Content-Type': 'application/soap+xml; charset=utf-8',
-  };
-  return fetch(url, { method: 'POST', body, headers, redirect: 'follow' });
+import got from 'got';
+
+export function uupPostRequest(requestURL: string, body: string) {
+  return got.post(requestURL, {
+    body,
+    headers: {
+      // user agent to mimic an windows device
+      'User-Agent': 'Windows-Update-Agent/10.0.10011.16384 Client-Protocol/2.50',
+      'Content-Type': 'application/soap+xml; charset=utf-8',
+    },
+    https: {
+      // disable ssl certificate verification
+      rejectUnauthorized: false,
+    },
+  });
 }
