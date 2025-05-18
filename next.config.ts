8e165f16e32f23ab5d503a85599e27c213cdf6a9
Avirup Ghosh <oviirup@gmail.com>
2025-05-18 23:21:29 +0530

    🔧 added sprite config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
-import '@/env'
+import { build } from '@oviirup/sprite'
+import { isDev } from '@/env'
 import type { NextConfig } from 'next'
+
+// build sprite files once
+if (!process.env.sprite_build_complete) {
+  build({ watch: isDev })
+  process.env.sprite_build_complete = 'TRUE'
+}
 
 const config: NextConfig = {
   // disable eslint & typescript during build
